2eef781005d966354c71587e04066cf99a1857c1
v15a1 <visalrajapakse@gmail.com>
2019-07-11 17:57:44 +0530

    added sorting to reastaurant page

--- student-four/EXTRA PAGES/restaurantJavascript.js
+++ student-four/EXTRA PAGES/restaurantJavascript.js
@@ -6,49 +6,6 @@ function changeDiningOption(){
     //console.log("the dining location is " + diningLocation);
     switch(diningOption){
         case "Restaurant":
-            console.log("inside Restaurant case");
-            document.getElementById("fastFoodrestaurant1").style.display="none";
-            document.getElementById("fastFoodrestaurant2").style.display="none";
-            document.getElementById("fastFoodrestaurant3").style.display="none";
-            document.getElementById("candyShop").style.display="none";
-            document.getElementById("restaurant1").style.display="block";
-            document.getElementById("vegetarianRestaurant").style.display="block";
-            document.getElementById("iceCreamShop").style.display="none";
-
-            return;
-        case "Buffet":
-            console.log("inside Buffet case");
-            document.getElementById("fastFoodrestaurant1").style.display="none";
-            document.getElementById("fastFoodrestaurant2").style.display="none";
-            document.getElementById("fastFoodrestaurant3").style.display="none";
-            document.getElementById("iceCreamShop").style.display="none";
-            document.getElementById("candyShop").style.display="block";
-            return
-        case "Vegetarian choices":
-            console.log("inside Vegetarian case");
-            document.getElementById("fastFoodrestaurant1").style.display="none";
-            document.getElementById("fastFoodrestaurant2").style.display="none";
-            document.getElementById("fastFoodrestaurant3").style.display="none";
-            document.getElementById("candyShop").style.display="none";
-            document.getElementById("restaurant1").style.display="none";
-            document.getElementById("iceCreamShop").style.display="none";
-            document.getElementById("vegetarianRestaurant").style.gridColumn = "1/5"    //manipulating grid layout using javascript
-            return  
-        case "Fast-food outlets":
-            console.log("inside Fast-food case");
-            document.getElementById("candyShop").style.display="none";
-            document.getElementById("iceCreamShop").style.display="none";
-            document.getElementById("candyShop").style.display="none";
-            document.getElementById("restaurant1").style.display="none";
-            document.getElementById("vegetarianRestaurant").style.display="none";
-            return
-        case "Ice cream and Sweets":
-            console.log("inside Ice cream and Sweets case");
-            document.getElementById("fastFoodrestaurant1").style.display="none";
-            document.getElementById("fastFoodrestaurant2").style.display="none";
-            document.getElementById("fastFoodrestaurant3").style.display="none";
-            document.getElementById("restaurant1").style.display="none";
-            document.getElementById("vegetarianRestaurant").style.display="none";
-            return
+            console.log("inside Restaurant")
     }
 }

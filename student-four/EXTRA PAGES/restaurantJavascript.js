d5cef17ea9736a2a398c9a59e039a00f048ed063
v15a1 <visalrajapakse@gmail.com>
2019-07-11 21:09:18 +0530

    added logo to common-resources

--- student-four/EXTRA PAGES/restaurantJavascript.js
+++ student-four/EXTRA PAGES/restaurantJavascript.js
@@ -1,11 +1,73 @@
 function changeDiningOption(){
     var diningOption;
-    diningOption = document.getElementById("select-type").value;
+    diningOption = document.getElementById("diningOption").value;
     //diningLocation = document.getElementById("select-location").value;
     console.log("the dining option is " + diningOption);
     //console.log("the dining location is " + diningLocation);
     switch(diningOption){
+        case "SelectAnOption":
+            console.log("inside Buffet case");
+            document.getElementById("fastFood1").style.display = "block";
+            document.getElementById("icecreamShop").style.display = "block";
+            document.getElementById("fastFood2").style.display = "block";
+            document.getElementById("restaurant1").style.display = "block";
+            document.getElementById("restaurant2").style.display = "block";
+            document.getElementById("buffetRestaurant").style.display = "block";
+            document.getElementById("candyShop").style.display = "block";
+            break;
+        case "Buffet":
+            console.log("inside Buffet case");
+            document.getElementById("fastFood1").style.display = "none";
+            document.getElementById("icecreamShop").style.display = "none";
+            document.getElementById("fastFood2").style.display = "none";
+            document.getElementById("restaurant1").style.display = "none";
+            document.getElementById("restaurant2").style.display = "none";
+            document.getElementById("buffetRestaurant").style.display = "block";
+            document.getElementById("candyShop").style.display = "none";
+            break;
+
         case "Restaurant":
-            console.log("inside Restaurant")
+            console.log("inside Restaurant case");
+            document.getElementById("fastFood1").style.display = "none";
+            document.getElementById("icecreamShop").style.display = "none";
+            document.getElementById("fastFood2").style.display = "none";
+            document.getElementById("restaurant1").style.display = "block";
+            document.getElementById("restaurant2").style.display = "block";
+            document.getElementById("buffetRestaurant").style.display = "block";
+            document.getElementById("candyShop").style.display = "none";   
+            break;
+
+        case "Vegetarian":
+            console.log("inside Restaurant case");
+            document.getElementById("fastFood1").style.display = "none";
+            document.getElementById("icecreamShop").style.display = "none";
+            document.getElementById("fastFood2").style.display = "none";
+            document.getElementById("restaurant1").style.display = "none";
+            document.getElementById("restaurant2").style.display = "block";
+            document.getElementById("buffetRestaurant").style.display = "block";
+            document.getElementById("candyShop").style.display = "none";   
+            break;   
+
+        case "Candy":
+            console.log("inside Restaurant case");
+            document.getElementById("fastFood1").style.display = "none";
+            document.getElementById("icecreamShop").style.display = "none";
+            document.getElementById("fastFood2").style.display = "none";
+            document.getElementById("restaurant1").style.display = "none";
+            document.getElementById("restaurant2").style.display = "none";
+            document.getElementById("buffetRestaurant").style.display = "none";
+            document.getElementById("candyShop").style.display = "block";   
+            break;
+
+        case "Icecream":
+            console.log("inside Restaurant case");
+            document.getElementById("fastFood1").style.display = "none";
+            document.getElementById("icecreamShop").style.display = "block";
+            document.getElementById("fastFood2").style.display = "none";
+            document.getElementById("restaurant1").style.display = "none";
+            document.getElementById("restaurant2").style.display = "none";
+            document.getElementById("buffetRestaurant").style.display = "none";
+            document.getElementById("candyShop").style.display = "none";   
+            break;       
     }
 }
